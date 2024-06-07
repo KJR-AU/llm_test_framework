735e78d89a33cc3a333bd28dca0a0b52d58bfcda
KjrJoe <joe.burton@kjr.com.au>
2024-06-08 08:24:24 +1000

    setup targets module

---
+++ src/targets/__init__.py
@@ -0,0 +1,9 @@
+from .Target import Target
+from .LangChainTarget import LangChainTarget
+from .LlamaIndexTarget import LlamaIndexTarget
+
+__all__ = [
+    "Target",
+    "LangChainTarget",
+    "LlamaIndexTarget"
+]
