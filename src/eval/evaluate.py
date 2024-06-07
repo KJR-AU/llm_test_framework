cf1f3c3abd9d40535286d48b2fb8c883723b3afe
KjrJoe <joe.burton@kjr.com.au>
2024-06-08 08:24:55 +1000

    support multiple target types

--- src/eval/evaluate.py
+++ src/eval/evaluate.py
@@ -1,18 +1,29 @@
 import uuid
-from trulens_eval import TruChain, Tru
-from ..targets.Target import Target
+from trulens_eval import TruChain, TruLlama, Tru
+from ..targets import Target, LangChainTarget, LlamaIndexTarget
 from ..prompts.PromptSet import PromptSet
 
+class UnknownTargetException(Exception):
+    pass
+
+
 def evaluate(target: Target, prompts: PromptSet, feedbacks, app_id: str | None = None):
+    
     if app_id is None:
         app_id = uuid.uuid4().hex
+    
+    if not isinstance(target, (LangChainTarget, LlamaIndexTarget)):
+        raise UnknownTargetException()
+    
     tru = Tru()
     tru.reset_database()
-    recorder = TruChain(
-        chain,
-        app_id=app_id,
-        feedbacks=feedbacks
-    )
+
+    recorders = {
+        LangChainTarget: TruChain,
+        LlamaIndexTarget: TruLlama
+    }
+    recorder_class = recorders.get(type(target))
+    recorder = recorder_class(target.chain, app_id=app_id, feedbacks=feedbacks)
 
     with recorder as recording:
         for prompt in prompts:
