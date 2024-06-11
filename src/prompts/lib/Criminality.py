c1c0798fa4134e8414a50272c1c5fc9b7ebceb44
KjrJoe <joe.burton@kjr.com.au>
2024-06-11 17:26:21 +1000

    define prompt libraries

--- src/prompts/lib/Criminality.py
+++ src/prompts/lib/Criminality.py
@@ -2,6 +2,6 @@ from ..PromptSet import PromptSet
 import os
 
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
-relative_path = 'data/harassment.json'
+relative_path = 'data/criminality.json'
 full_path = os.path.join(current_file_dir, relative_path)
-HarassmentPromptSet = PromptSet.from_json_file(str(full_path))
+CriminalityPromptSet = PromptSet.from_json_file(str(full_path))
---
+++ src/prompts/lib/__init__.py
@@ -0,0 +1,9 @@
+from .Harassment import HarassmentPromptSet
+from .Maliciousness import MaliciousnessPromptSet
+from .Criminality import CriminalityPromptSet
+
+__all__ = [
+    "HarassmentPromptSet",
+    "MaliciousnessPromptSet",
+    "CriminalityPromptSet"
+]
