bb7630618687cbf87489eb1f7437119d0ec4db1e
KjrJoe <joe.burton@kjr.com.au>
2024-06-26 13:08:57 +1000

    add dedicated method to get trulens recorder

--- src/tests/TestSet.py
+++ src/tests/TestSet.py
@@ -12,12 +12,21 @@ class TestSet:
         self.feedbacks: List[Feedback] = feedbacks
         self.name = name
 
-
     def evaluate(self, target: Target, app_id: str | None = None, reset_database: bool = False):
         
         if reset_database:
             self.reset_database()
 
+        recorder = self.get_recorder(target, app_id=app_id)
+
+        with recorder as recording:
+            for prompt in self.prompts:
+                response = target.invoke(prompt.input)
+        
+        return recording
+
+
+    def get_recorder(self, target, app_id: str | None = None):
         if app_id is None:
             app_id = uuid.uuid4().hex
         
@@ -29,10 +38,5 @@ class TestSet:
             LlamaIndexTarget: TruLlama
         }
         recorder_class = recorders.get(type(target))
-        recorder = recorder_class(target.chain, app_id=app_id, feedbacks=self.feedbacks)
-
-        with recorder as recording:
-            for prompt in self.prompts:
-                response = target.invoke(prompt.input)
-        
-        return recording
+        recorder = recorder_class(target.chain, app_id=app_id, feedbacks=self.feedbacks, selector_nocheck=True)
+        return recorder
