cbf0ea79c9a4689a18a3ae8c409b4e556c4546b7
KjrJoe <joe.burton@kjr.com.au>
2024-06-26 15:38:10 +1000

    enable setting feedback provider at TestSet level

--- src/tests/TestSet.py
+++ src/tests/TestSet.py
@@ -4,20 +4,32 @@ from ..targets import Target, LangChainTarget, LlamaIndexTarget
 from ..prompts.PromptSet import PromptSet
 from typing import List
 from .UnknownTargetException import UnknownTargetException
+from ..metrics.Metric import Metric
 
 class TestSet:
 
-    def __init__(self, prompts: PromptSet, feedbacks: List[Feedback], name: str = ""):
+    SUPPORTED_PROVIDERS = [
+        "openai",
+        "llama3"
+    ]
+
+    def __init__(self, prompts: PromptSet, feedbacks: List[Feedback], name: str = "", provider: str = None):
         self.prompts: PromptSet = prompts
         self.feedbacks: List[Feedback] = feedbacks
-        self.name = name
+        self.name: str = name
+        self.provider = provider
+            
 
     def evaluate(self, target: Target, app_id: str | None = None, reset_database: bool = False):
+        """
+        """
         
+        feedbacks = self._get_coerced_feedbacks()
+
         if reset_database:
             self.reset_database()
 
-        recorder = self.get_recorder(target, app_id=app_id)
+        recorder = self.get_recorder(target, feedbacks, app_id=app_id)
 
         with recorder as recording:
             for prompt in self.prompts:
@@ -26,7 +38,7 @@ class TestSet:
         return recording
 
 
-    def get_recorder(self, target, app_id: str | None = None):
+    def get_recorder(self, target: Target, feedbacks: List[Feedback], app_id: str | None = None):
         if app_id is None:
             app_id = uuid.uuid4().hex
         
@@ -38,5 +50,39 @@ class TestSet:
             LlamaIndexTarget: TruLlama
         }
         recorder_class = recorders.get(type(target))
-        recorder = recorder_class(target.chain, app_id=app_id, feedbacks=self.feedbacks, selector_nocheck=True)
+        recorder = recorder_class(target.chain, app_id=app_id, feedbacks=feedbacks, selector_nocheck=True)
         return recorder
+
+    
+    @property
+    def provider(self):
+        return self._provider
+
+    @provider.setter
+    def provider(self, provider: str | None):
+        if provider not in [None, *self.SUPPORTED_PROVIDERS]:
+            supported_provider_string: str = ", ".join(self.SUPPORTED_PROVIDERS)
+            raise ValueError(f"unsupported provider: '{self.provider}'. Provider must be None or one of {supported_provider_string}")
+        self._provider = provider
+
+
+    def _get_coerced_feedbacks(self):
+        """Returns a list of trulens-compatible feedback functions. Attempts 
+        to use a user-set provider attribute to extract the feedback function
+        from a Metric object.
+        """
+        coerced_feedbacks = []
+        for feedback in self.feedbacks:
+            
+            # Try to set the provider if no provider has been set
+            if isinstance(feedback, Metric):
+                if self.provider is None:
+                    raise ValueError("provider cannot be determined")
+                coerced_feedback = getattr(feedback, self.provider)
+            elif isinstance(feedback, Feedback):
+                coerced_feedback = feedback
+            else:
+                raise TypeError("unrecognised feedback type")
+            
+            coerced_feedbacks.append(coerced_feedback)
+        return coerced_feedbacks
